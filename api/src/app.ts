7509ee02ac6c3e281afacf8180f3805254858de0
Azorimor <azorimor@gmail.com>
2021-03-15 14:55:56 +0100

    Only use http logger in production.

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,13 +4,16 @@ import helmet from 'helmet';
 import pino from './util/logger';
 import logger from 'pino-http';
 import router from './router/router';
+import { PROD } from './util/config';
 
 const app: Application = express();
 
 // Middlewares
 app.use(cors());
 app.use(helmet());
-app.use(logger({ logger: pino }));
+if (PROD) {
+  app.use(logger({ logger: pino }));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
